4fc2c74937ca3c334c8c7242f306edb48472dbe8
LuisArostegui <luisarosteguiruizit@gmail.com>
2021-10-22 13:52:58 +0200

    Edito atributos actividad.py. Closes #15

--- sportcar/actividad.py
+++ sportcar/actividad.py
@@ -1,13 +1,29 @@
-from datetime import datetime
-
 
 class Actividad:
 
-    def __init__(self, nombre, fecha_inicio, fecha_final, tipo, intensidad, localizacion, ciudad):
-        self.nombre = nombre
-        self.fecha_inicio = fecha_inicio
-        self.fecha_final = fecha_final
-        self.tipo = tipo
-        self.ciudad = ciudad
-        self.intensidad = intensidad
-        self.localizacion = localizacion
+    def __init__(self, nombre, fecha_inicio, fecha_final, tipo, ubicacion, ciudad):
+        self._nombre = nombre
+        self._fecha_inicio = fecha_inicio
+        self._fecha_final = fecha_final
+        self._tipo = tipo
+        self._ciudad = ciudad
+        self._ubicacion = ubicacion
+
+    def get_nombre(self):
+        return self._nombre
+
+    def get_fecha_inicio(self):
+        return self._fecha_inicio
+
+    def get_fecha_final(self):
+        return self._fecha_final
+
+    def get_ciudad(self):
+        return self._ciudad
+
+    def get_tipo(self):
+        return self._ciudad
+
+    def get_ubicacion(self):
+        return self._ubicacion
+
