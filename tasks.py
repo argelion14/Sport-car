3413459960ad7c53cfcaf9067e04ca4c8f4950ff
argelion14 <argelion14@gmail.com>
2021-12-26 12:46:50 +0100

    Le añado la tarea task (close #18)

--- tasks.py
+++ tasks.py
@@ -5,3 +5,9 @@ def check(c):
     print("Comprobar la compilación")
     run ("python3 -m compileall sportcar/*.py")
     print("Compilacion nice!")
+
+@task
+def test(c):
+    print("Lanzamos los test")
+    run("pytest test/")
+    print("Todo correcto")
