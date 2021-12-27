10d2c9858181b2ac153fdc4f45b72374fe51f849
argelion14 <argelion14@gmail.com>
2021-12-27 18:02:26 +0100

    Test de uniitest que me servirá para comprobar el código Test en uniitest que me sirve para comprobar el correcto uso de los errores que existen en mi código

--- test/test.py
+++ test/test.py
@@ -1,18 +1,26 @@
-import pytest
 import unittest
 import sys
-sys.path.append("./sportcar")
+sys.path.append(".")
 
+import sportcar.actividad
+import sportcar.usuario
+import sportcar.errores
 
-from ...sportcar import *
+class Tests (unittest.TestCase):
+    
+    def test_vehiculo(self):
+        """
+        Test para comprobar que se da la excepción cuando no se introduce un formato de vehículo correcto
+        """
+        with self.assertRaises(sportcar.errores.VehiculoFormatError):
+            usuario = sportcar.usuario.Usuario('Angel','NO','695641699','Granada')
+    
+    def test_telefono(self):
+        """
+        Test para comprobar que se da la excepción cuando el formato de telefono que se da no es correcto
+        """
+        with self.assertRaises(sportcar.errores.TelefonoFormatError):
+            usuario = sportcar.usuario.Usuario('Angel',True,695641699,'Granada')
 
-
-#from sportcar.actividad import Actividad
-
-
-
-
-print("HOLA")
-
-
-
+if __name__ == '__main__':
+    unittest.main()
