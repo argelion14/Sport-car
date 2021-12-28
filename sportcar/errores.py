a197c5ecf38cb107023f366cd124d4ac95883e59
argelion14 <argelion14@gmail.com>
2021-12-28 16:26:30 +0100

    Incorporación de los errores

--- sportcar/errores.py
+++ sportcar/errores.py
@@ -9,3 +9,15 @@ class VehiculoFormatError (Exception):
 class NombreFormatError (Exception):
     def __init__(self, message = "Debe ser un nombre admitido"):
         self.message = message
+
+class NombreUsuarioTooLongError (Exception):
+    def __init__(self, message = "Nombre demasiado largo"):
+        self.message = message
+
+class NombreUsuarioTooShortError (Exception):
+    def __init__(self, message = "Nombre demasiado corto"):
+        self.message = message
+
+class TipoActividadFormatError (Exception):
+    def __init__(self, message = "Debe ser un tipo admitido"):
+        self.message = message
