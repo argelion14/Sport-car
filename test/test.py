2a6504aa9d46c77e2b9e5b383856123cf52f47ac
argelion14 <argelion14@gmail.com>
2021-12-28 17:43:15 +0100

    Testeo lo nuevo añadido

--- test/test.py
+++ test/test.py
@@ -6,28 +6,23 @@ import sportcar.actividad
 import sportcar.usuario
 import sportcar.errores
 
-class Tests (unittest.TestCase):
+class TestActividad (unittest.TestCase):
     
-    def test_vehiculo(self):
-        """
-        Test para comprobar que se da la excepción cuando no se introduce un formato de vehículo correcto
-        """
-        with self.assertRaises(sportcar.errores.VehiculoFormatError):
-            usuario = sportcar.usuario.Usuario('Angel','NO','695641699','Granada')
-    
-    def test_telefono(self):
-        """
-        Test para comprobar que se da la excepción cuando el formato de telefono que se da no es correcto
-        """
-        with self.assertRaises(sportcar.errores.TelefonoFormatError):
-            usuario = sportcar.usuario.Usuario('Angel',True,695641699,'Granada')
-
     def test_nombreOpcion(self):
         """
         Test para comprobar que se levanta la excepción cuando se da un nombre que no se encuentra entre las opciones disponibles
         """
         with self.assertRaises(sportcar.errores.NombreFormatError):
             actividad = sportcar.actividad.Actividad('NATACiION','fecha_inicio', 'fecha_final', 'DEPORTIVA', 'ubicacion', 'ciudad')
+
+    def test_tipoOpcion(self):
+        """
+        Test para comprobar que se levanta la excepción cuando se da un tipo que no se encuentra entre las opciones disponibles
+        """
+        with self.assertRaises(sportcar.errores.TipoActividadFormatError):
+            actividad = sportcar.actividad.Actividad('NATACION','fecha_inicio', 'fecha_final', 'DEPORTiIVA', 'ubicacion', 'ciudad')
+
+class TestUsuario (unittest.TestCase):
 
     def test_nombreUsuarioLargo(self):
         """
@@ -43,12 +38,19 @@ class Tests (unittest.TestCase):
         with self.assertRaises(sportcar.errores.NombreUsuarioTooShortError):
             usuario = sportcar.usuario.Usuario('J',True,'695641699','Granada')
 
-    def test_tipoOpcion(self):
+    def test_vehiculo(self):
         """
-        Test para comprobar que se levanta la excepción cuando se da un tipo que no se encuentra entre las opciones disponibles
+        Test para comprobar que se da la excepción cuando no se introduce un formato de vehículo correcto
         """
-        with self.assertRaises(sportcar.errores.TipoActividadFormatError):
-            actividad = sportcar.actividad.Actividad('NATACION','fecha_inicio', 'fecha_final', 'DEPORTiIVA', 'ubicacion', 'ciudad')
+        with self.assertRaises(sportcar.errores.VehiculoFormatError):
+            usuario = sportcar.usuario.Usuario('Angel','NO','695641699','Granada')
+    
+    def test_telefono(self):
+        """
+        Test para comprobar que se da la excepción cuando el formato de telefono que se da no es correcto
+        """
+        with self.assertRaises(sportcar.errores.TelefonoFormatError):
+            usuario = sportcar.usuario.Usuario('Angel',True,695641699,'Granada')
 
 if __name__ == '__main__':
     unittest.main()
