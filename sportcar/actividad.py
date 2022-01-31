7a60499247c3ec665a02ffefa8444ca4067a4be1
argelion14 <argelion14@gmail.com>
2022-01-31 19:18:39 +0100

    (close #38)

--- sportcar/actividad.py
+++ sportcar/actividad.py
@@ -14,7 +14,28 @@ class TipoActividad(Enum):
     APRENDIZAJE = "Actividad en la que se prioriza el querer aprender"
 
 class Actividad:
-
+    """
+    Clase que define un usuario
+    Attributes
+    ----------
+    _nombre : nombre
+        Nombre de la actividad
+    _fecha_inicio : datetime
+        Inicio de la actividad
+    _fecha_final : datetime
+        Final de la actividad
+    _tipo : TipoActividad
+        Tipo de la actividad
+    _ubicación : str
+        Lugar donde se realizará la actividad
+    _ciudad : str
+        Ciudad donde se realizará la actividad
+    Methods
+    -------
+    get_nombre
+        Devuelve el nombre de la actividad
+    
+    """
     def __init__(self, nombre, fecha_inicio, fecha_final, tipo, ubicacion, ciudad):
 
         if (nombre not in ActividadesDisponibles._member_names_):
@@ -33,19 +54,37 @@ class Actividad:
         self._ubicacion = ubicacion
 
     def get_nombre(self):
+        """
+        Devuelve el nombre de la actividad
+        """
         return self._nombre
 
     def get_fecha_inicio(self):
+        """
+        Devuelve la fecha de inicio de la actividad
+        """
         return self._fecha_inicio
 
     def get_fecha_final(self):
+        """
+        Devuelve la fecha final de la actividad
+        """
         return self._fecha_final
 
     def get_ciudad(self):
+        """
+        Devuelve la ciudad donde se realiza la actividad
+        """
         return self._ciudad
 
     def get_tipo(self):
+        """
+        Devuelve el tipo de actividad
+        """
         return self._ciudad
 
     def get_ubicacion(self):
+        """
+        Devuelve la ubicacion donde se realiza la actividad
+        """
         return self._ubicacion
